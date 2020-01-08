d7d53dcafd7e84f62ad77d25b8914395f0462e78
E-no <632451830@qq.com>
2020-01-08 07:39:31 +0000

    管理后台 - 拒绝退款

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Exceptions\InvalidRequestException;
+use App\Http\Requests\Admin\HandleRefundRequest;
 use App\Http\Requests\Request;
 use App\Models\Order;
 use Encore\Admin\Controllers\AdminController;
@@ -39,10 +40,10 @@ class OrdersController extends AdminController
         $grid->column('user.name', '买家');
         $grid->total_amount('总金额')->sortable();
         $grid->paid_at('支付时间')->sortable();
-        $grid->ship_status('物流')->display(function($value) {
+        $grid->ship_status('物流')->display(function ($value) {
             return Order::$shipStatusMap[$value];
         });
-        $grid->refund_status('退款状态')->display(function($value) {
+        $grid->refund_status('退款状态')->display(function ($value) {
             return Order::$refundStatusMap[$value];
         });
         // 禁用创建按钮，后台不需要创建订单
@@ -130,14 +131,14 @@ class OrdersController extends AdminController
             ->body(view('admin.orders.show', ['order' => Order::find($id)]));
     }
 
-    public function ship(Order $order,Request $request)
+    public function ship(Order $order, Request $request)
     {
         // 判断当前订单是否已支付
-        if(!$order->paid_at){
+        if (!$order->paid_at) {
             throw new InvalidRequestException('该订单未付款');
         }
         // 判断当前订单状态是否为未发货
-        if($order->ship_status !== Order::SHIP_STATUS_PENDING){
+        if ($order->ship_status !== Order::SHIP_STATUS_PENDING) {
             throw new InvalidRequestException('该订单已发货');
         }
         // Laravel 5.5 之后 validate 方法可以返回校验过的值
@@ -158,5 +159,26 @@ class OrdersController extends AdminController
 
         // 返回上一页
         return redirect()->back();
+    }
+
+    public function handleRefund(Order $order, HandleRefundRequest $request)
+    {
+        if ($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+
+        if($request->input('agree')){
+
+        }else{
+            // 将拒绝退款理由放到订单的 extra 字段中
+            $extra = $order->extra ?: [];
+            $extra['refund_disagree_reason'] = $request->input('reason');
+            // 将订单的退款状态改为未退款
+            $order->update([
+                'refund_status' => Order::REFUND_STATUS_PENDING,
+                'extra'         => $extra,
+            ]);
+        }
+        return $order;
     }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -11,6 +11,27 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|OrderItem newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|OrderItem query()
  * @mixin \Eloquent
+ * @property int $id
+ * @property int $order_id
+ * @property int $product_id
+ * @property int $product_sku_id
+ * @property int $amount
+ * @property float $price
+ * @property int|null $rating
+ * @property string|null $review
+ * @property \Illuminate\Support\Carbon|null $reviewed_at
+ * @property-read \App\Models\Order $order
+ * @property-read \App\Models\Product $product
+ * @property-read \App\Models\ProductSku $productSku
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrderItem whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrderItem whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrderItem whereOrderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrderItem wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrderItem whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrderItem whereProductSkuId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrderItem whereRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrderItem whereReview($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrderItem whereReviewedAt($value)
  */
 class OrderItem extends Model
 {
