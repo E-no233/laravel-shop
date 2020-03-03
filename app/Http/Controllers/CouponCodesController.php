6e6c72da5584e36897a3675d2c050eebec434adf
E-no <632451830@qq.com>
2020-03-03 09:13:26 +0000

    优化优惠券模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -9,13 +9,13 @@ use Illuminate\Http\Request;
 
 class CouponCodesController extends Controller
 {
-    public function show($code)
+    public function show($code,Request $request)
     {
         $record = CouponCode::where('code', $code)->first();
         if (!$record) {
             throw new CouponCodeUnavailableException('优惠券不存在');
         }
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
         return $record;
     }
 }
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -28,40 +28,56 @@
                                     </div>
                                     <div>
               <span class="product-title">
-                 <a target="_blank" href="{{ route('products.show', [$item->product_id]) }}">{{ $item->product->title }}</a>
+                 <a target="_blank"
+                    href="{{ route('products.show', [$item->product_id]) }}">{{ $item->product->title }}</a>
               </span>
                                         <span class="sku-title">{{ $item->productSku->title }}</span>
                                     </div>
                                 </td>
                                 <td class="sku-price text-center vertical-middle">￥{{ $item->price }}</td>
                                 <td class="sku-amount text-center vertical-middle">{{ $item->amount }}</td>
-                                <td class="item-amount text-right vertical-middle">￥{{ number_format($item->price * $item->amount, 2, '.', '') }}</td>
+                                <td class="item-amount text-right vertical-middle">
+                                    ￥{{ number_format($item->price * $item->amount, 2, '.', '') }}</td>
                             </tr>
                         @endforeach
-                        <tr><td colspan="4"></td></tr>
+                        <tr>
+                            <td colspan="4"></td>
+                        </tr>
                     </table>
                     <div class="order-bottom">
                         <div class="order-info">
-                            <div class="line"><div class="line-label">收货地址：</div><div class="line-value">{{ implode(' ', $order->address) }}</div></div>
-                            <div class="line"><div class="line-label">订单备注：</div><div class="line-value">{{ $order->remark ?: '-' }}</div></div>
-                            <div class="line"><div class="line-label">订单编号：</div><div class="line-value">{{ $order->no }}</div></div>
+                            <div class="line">
+                                <div class="line-label">收货地址：</div>
+                                <div class="line-value">{{ implode(' ', $order->address) }}</div>
+                            </div>
+                            <div class="line">
+                                <div class="line-label">订单备注：</div>
+                                <div class="line-value">{{ $order->remark ?: '-' }}</div>
+                            </div>
+                            <div class="line">
+                                <div class="line-label">订单编号：</div>
+                                <div class="line-value">{{ $order->no }}</div>
+                            </div>
                             <!-- 输出物流状态 -->
                             <div class="line">
                                 <div class="line-label">物流状态：</div>
-                                <div class="line-value">{{ \App\Models\Order::$shipStatusMap[$order->ship_status] }}</div>
+                                <div
+                                    class="line-value">{{ \App\Models\Order::$shipStatusMap[$order->ship_status] }}</div>
                             </div>
                             <!-- 如果有物流信息则展示 -->
                             @if($order->ship_data)
                                 <div class="line">
                                     <div class="line-label">物流信息：</div>
-                                    <div class="line-value">{{ $order->ship_data['express_company'] }} {{ $order->ship_data['express_no'] }}</div>
+                                    <div
+                                        class="line-value">{{ $order->ship_data['express_company'] }} {{ $order->ship_data['express_no'] }}</div>
                                 </div>
                             @endif
-                            <!-- 订单已支付，且退款状态不是未退款时展示退款信息 -->
+                        <!-- 订单已支付，且退款状态不是未退款时展示退款信息 -->
                             @if($order->paid_at && $order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
                                 <div class="line">
                                     <div class="line-label">退款状态：</div>
-                                    <div class="line-value">{{ \App\Models\Order::$refundStatusMap[$order->refund_status] }}</div>
+                                    <div
+                                        class="line-value">{{ \App\Models\Order::$refundStatusMap[$order->refund_status] }}</div>
                                 </div>
                                 <div class="line">
                                     <div class="line-label">退款理由：</div>
@@ -70,6 +86,14 @@
                             @endif
                         </div>
                         <div class="order-summary text-right">
+                            <!-- 展示优惠信息开始 -->
+                            @if($order->couponCode)
+                                <div class="text-primary">
+                                    <span>优惠信息：</span>
+                                    <div class="value">{{ $order->couponCode->description }}</div>
+                                </div>
+                            @endif
+                            <!-- 展示优惠信息结束 -->
                             <div class="total-amount">
                                 <span>订单总价：</span>
                                 <div class="value">￥{{ $order->total_amount }}</div>
@@ -96,25 +120,27 @@
                                     <div class="value">{{ $order->extra['refund_disagree_reason'] }}</div>
                                 </div>
                             @endif
-                            <!-- 支付按钮开始 -->
+                        <!-- 支付按钮开始 -->
                             @if(!$order->paid_at && !$order->closed)
                                 <div class="payment-buttons">
-                                    <a class="btn btn-primary btn-sm" href="{{ route('payment.alipay', ['order' => $order->id]) }}">支付宝支付</a>
+                                    <a class="btn btn-primary btn-sm"
+                                       href="{{ route('payment.alipay', ['order' => $order->id]) }}">支付宝支付</a>
                                 </div>
                             @endif
-                            <!-- 支付按钮结束 -->
+                        <!-- 支付按钮结束 -->
                             <!-- 如果订单的发货状态为已发货则展示确认收货按钮 -->
                             @if($order->ship_status === \App\Models\Order::SHIP_STATUS_DELIVERED)
                                 <div class="receive-button">
                                     <form method="post" action="{{ route('orders.received', [$order->id]) }}">
                                         <!-- csrf token 不能忘 -->
-                                        {{ csrf_field() }}
-                                        <!-- 将原本的表单替换成下面这个按钮 -->
-                                        <button type="button" id="btn-receive" class="btn btn-sm btn-success">确认收货</button>
+                                    {{ csrf_field() }}
+                                    <!-- 将原本的表单替换成下面这个按钮 -->
+                                        <button type="button" id="btn-receive" class="btn btn-sm btn-success">确认收货
+                                        </button>
                                     </form>
                                 </div>
                             @endif
-                            <!-- 订单已支付，且退款状态是未退款时展示申请退款按钮 -->
+                        <!-- 订单已支付，且退款状态是未退款时展示申请退款按钮 -->
                             @if($order->paid_at && $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
                                 <div class="refund-button">
                                     <button class="btn btn-sm btn-danger" id="btn-apply-refund">申请退款</button>
@@ -130,7 +156,7 @@
 
 @section('scriptsAfterJs')
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             // 退款按钮点击事件
             $('#btn-apply-refund').click(function () {
                 swal({
@@ -138,7 +164,7 @@
                     content: "input",
                 }).then(function (input) {
                     // 当用户点击 swal 弹出框上的按钮时触发这个函数
-                    if(!input) {
+                    if (!input) {
                         swal('退款理由不可空', '', 'error');
                         return;
                     }
@@ -153,7 +179,7 @@
                 });
             });
             // 确认收货按钮点击事件
-            $('#btn-receive').click(function() {
+            $('#btn-receive').click(function () {
                 // 弹出确认框
                 swal({
                     title: "确认已经收到商品？",
@@ -161,7 +187,7 @@
                     dangerMode: true,
                     buttons: ['取消', '确认收到'],
                 })
-                    .then(function(ret) {
+                    .then(function (ret) {
                         // 如果点击取消按钮则不做任何操作
                         if (!ret) {
                             return;
